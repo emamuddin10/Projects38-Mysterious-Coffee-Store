6113960926ff76884d9306e554508f377aa32ba1
Emam Uddin <emamskingdom@gmail.com>
2025-03-17 17:34:08 +0600

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -8,25 +8,45 @@ import {
 } from "react-router-dom";
 import AddCoffee from './components/AddCoffee.jsx';
 import UpdateCoffee from './components/UpdateCoffee.jsx';
+import HomeLayout from './components/HomeLayout.jsx';
+import SignUp from './components/SignUp.jsx';
+import Signin from './components/Signin.jsx';
+// import Home from './components/Home.jsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App></App>,
-    loader: ()=> fetch('http://localhost:5000/coffee')
-  },
-  {
-    path:'/addCoffee',
-    element:<AddCoffee></AddCoffee>
-  },
-  {
-    path:'/updateCoffee/:id',
-    element:<UpdateCoffee></UpdateCoffee>,
-    loader:({params})=> fetch(`http://localhost:5000/coffee/${params.id}`)
-  },
+    path:'/',
+    element:<HomeLayout></HomeLayout>,
+    children:[
+      {
+        path: "/",
+        element: <App></App>,
+        loader: ()=> fetch('http://localhost:5000/coffee')
+      },
+      {
+        path:'/addCoffee',
+        element:<AddCoffee></AddCoffee>
+      },
+      {
+        path:'/updateCoffee/:id',
+        element:<UpdateCoffee></UpdateCoffee>,
+        loader:({params})=> fetch(`http://localhost:5000/coffee/${params.id}`)
+      },
+      {
+        path:'signUp',
+        element:<SignUp></SignUp>
+      },
+      {
+        path:'signIn',
+        element:<Signin></Signin>
+      }
+    ]
+  }
 
 
 ]);
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
