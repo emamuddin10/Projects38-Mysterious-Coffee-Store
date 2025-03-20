ee6afef031d8b58146ba1d527f5348be95e68126
Emam Uddin <emamskingdom@gmail.com>
2025-03-20 15:59:46 +0600

    updated categiory

--- src/components/UpdateCoffee.jsx
+++ src/components/UpdateCoffee.jsx
@@ -19,7 +19,7 @@ const UpdateCoffee = () => {
     
         const updatedCoffee = {name,chef,category,quantity,teste,details,photo}
         console.log(updatedCoffee)
-        fetch(`http://localhost:5000/coffee/${_id}`, {
+        fetch(`https://projects38-mysterious-coffee-server-h86vjspxt.vercel.app/coffee/${_id}`, {
           method:"PUT",
           headers:{
             'content-type':'application/json'
